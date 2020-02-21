e448feda3e624aa7feea8187ed7c9ed51cf1b6e9
Onischuck Mikhail <onischuck.misha@yandex.ru>
2020-02-22 01:10:55 +0300

    Small changes for proposed grammar + some weird samples

---
+++ test_samples/AAA.java
@@ -0,0 +1,19 @@
+package com.abc.abc;
+
+import java.util.Set;
+
+public class AAA {
+  
+  class AAA_1 {
+    
+    public void main() {
+      ;
+    }
+    
+  }
+  
+  public void main() {
+    ;
+  }
+  
+}
---
+++ test_samples/c_plus_plus.cpp
@@ -0,0 +1,25 @@
+class A {
+  class B {
+    class A {
+    };
+  };
+};
+
+namespace AAA {
+  namespace AAA {
+    namespace AAA {
+      int x;
+    }
+    int x;
+  }
+  int x;
+}
+
+void a() {
+  class aa {
+    void a() {
+      class aa {
+      };
+    }
+  };
+}
