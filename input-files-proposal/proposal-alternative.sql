8f25e2380db26dea89032be72546dae9f3a091e1
Onischuck Mikhail <onischuck.misha@yandex.ru>
2020-02-26 23:13:50 +0300

    Added alternative variation for SQL-like syntax

--- input-files-proposal/proposal-alternative.sql
+++ input-files-proposal/proposal-alternative.sql
@@ -1,6 +1,6 @@
 use fragment "imports-instrummentation-fragment-with-dependencies"
 
--- instrument_some_statements_at_s3_and_s4:
+-- name: instrument_some_statements_at_s3_and_s4
 instrument
   print_iteration($node),
   correct_variable_i,
@@ -8,4 +8,21 @@ instrument
 at
   [first] if("x == 10"...) -> [all] for(*) # before_body
 where
+  { class_name = "Main" } & { method_name = "main" }
+
+
+-- альтернатива
+instrument
+  [first] if_statement
+    :before
+      <- init_logging
+    :before_body
+      <- print_branch("first if")
+      <- connect_to_db
+
+  [all] for_statement("i = 10"...)
+    :after
+      <- print_message($pointcut, $class_name)
+
+in context
   { class_name = "Main" } & { method_name = "main" }
