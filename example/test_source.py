3e4a741b6fd513b3dafc70ac8d0c558f26e67321
Onischuck Mikhail <onischuck.misha@yandex.ru>
2020-05-12 22:43:11 +0300

    Unified inserting text in fragment examples for Java and Python

--- example/test_source.py
+++ example/test_source.py
@@ -22,19 +22,5 @@ class Main:
         if self.a >= self.b:
           if self.a < 0:
             print "Hello World!"
-"""
-        for row in keys:
-            rowa = Frame(self, bg='gray40')
-            rowb = Frame(self, bg='gray40')
-            for p1, p2, p3, color, ktype, func in row:
-                if ktype == FUN:
-                    a = lambda s=self, a=func: s.evalAction(a)
-                else:
-                    a = lambda s=self, k=func: s.keyAction(k)
-                SLabel(rowa, p2, p3)
-                Key(rowb, text=p1, bg=color, command=a)
-            rowa.pack(side=TOP, expand=YES, fill=BOTH)
-            rowb.pack(side=TOP, expand=YES, fill=BOTH)            
-"""
 
 Main().main({})
